47c0e808b3ea7862bf624a17298251c9dae80a96
marsn <mpuccio90@gmail.com>
2015-10-13 20:38:46 -0300

    models & admin registration

--- layers-pw/afip/admin.py
+++ layers-pw/afip/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import VerificatorAPI
 # Register your models here.
+
+admin.site.register(VerificatorAPI)
--- layers-pw/afip/models.py
+++ layers-pw/afip/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class VerificatorAPI(models.Model):
+    """
+    Initiate a Instance of an verificator to validate the CUIT verificator
+    Number
+    """
+    name = models.CharField(max_length=255)
+    date = models.DateTimeField('Creation Day')
+
+    def __unicode__(self):
+        return self.name
+
+    def add_one(self, num):
+        num += 1
+        return num
