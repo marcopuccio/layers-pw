a4ba71fad1a5d3c283a55cf3bfb02d8863396e5e
marsn <mpuccio90@gmail.com>
2015-10-14 01:02:00 -0300

    simple classView structure

--- layers-pw/afip/views.py
+++ layers-pw/afip/views.py
@@ -1,6 +1,35 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from .models import VerificatorAPI
 # Create your views here.
 
 class ApplicationIndexView(TemplateView):
+    """
+    Shows Index Page of Afip Verificator Application
+    """
     template_name="afip/index.html"
+
+
+class ResultsForQueryView(DetailView):
+    """
+    Calculates the verification digit and compares that with the response
+    of the VerificatorAPI.
+    """
+    verificator = VerificatorAPI.objects.all()[0]
+    generator_pattern = (5, 4, 3, 2, 7, 6, 5, 4, 3, 2)
+
+    def digit_generation(cuil):
+        """
+        Slice the taken cuil to create a verification digit using the pattern
+        to calculate it.
+        """
+        cuil = cuil[:10]
+
+    def validate_digit():
+        """
+        Compare the response of the VerificatorAPI with complete cuit
+        including the recently created verification digit. If both of them
+        have the same value, ResultsForQueryView will return a success message.
+        """
+        pass
+
