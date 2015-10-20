449afb396e8eab83e16c0635217df10411eadf03
marsn <mpuccio90@gmail.com>
2015-10-20 00:34:15 -0300

    view refactor

--- layers-pw/afip/urls.py
+++ layers-pw/afip/urls.py
@@ -15,10 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from afip.views import ApplicationIndexView, CuilQueryView, ResultsForQueryView
+from afip.views import CuilQueryView
 
 urlpatterns = [
-    url(r'^$', ApplicationIndexView.as_view(), name="home"),
-    url(r'^query', CuilQueryView.as_view(), name="query"),
-    url(r'^result', ResultsForQueryView.as_view(), name="result"),
+    url(r'^$', CuilQueryView.as_view(), name="cuil_query"),
 ]
--- layers-pw/afip/views.py
+++ layers-pw/afip/views.py
@@ -1,38 +1,21 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView, FormView, DetailView
+from django.core.urlresolvers import reverse
 
 from .models import VerificatorAPI
 from .forms import CuilForm 
 # Create your views here.
 
-class ApplicationIndexView(TemplateView):
-    """
-    Shows Index Page of Afip Verificator Application
-    """
-    template_name="afip/verification.html"
-
-    def get_context_data(self, **kwargs):
-        form = CuilForm()
-        context = super(ApplicationIndexView, self).get_context_data(**kwargs)
-        context['form'] = form
-        return context
-
 class CuilQueryView(FormView):
     template_name = "afip/verification.html"
     form_class = CuilForm
-    success_url= "result/"
 
-    def form_valid(self, form):
-        return super(CuilQueryView, self).form_valid(form)
+    def get_success_url(self):
+        url = reverse('cuil_query')
+        return "%s?cuil=%s" % (url, self.request.POST['cuil'])
 
-    def get_context_data(self, **kwargs):
+    def form_valid(self, form, *args, **kwargs):
+        self.cuil = form.cleaned_data.get('cuil')
         context = super(CuilQueryView, self).get_context_data(**kwargs)
-        context['success'] = self.success
-        return context
-
-class ResultsForQueryView(TemplateView):
-    """
-    Calculates the verification digit and compares that with the response
-    of the VerificatorAPI.
-    """
-    template_name = "afip/result.html"
+        context['cuil'] = self.cuil
+        return super(CuilQueryView, self).form_valid(form, *args, **kwargs)
