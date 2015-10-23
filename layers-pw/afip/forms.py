30779ba68dada507df2fa51bd5881c5f3c7cbbf3
marsn <mpuccio90@gmail.com>
2015-10-23 00:06:00 -0300

    customizing form

--- layers-pw/afip/forms.py
+++ layers-pw/afip/forms.py
@@ -1,4 +1,11 @@
 from django import forms
 
 class CuilForm(forms.Form):
-    cuil = forms.CharField(max_length=11)
+    cuil = forms.CharField(label="Ingrese su CUIL",
+                           error_messages= {
+                               'required': 'Este campo es obligatorio',
+                               'min_length': 'Debe ingresar minimo 11 digitos',
+                               },
+                           min_length=11,
+                           max_length=11,
+                           )
